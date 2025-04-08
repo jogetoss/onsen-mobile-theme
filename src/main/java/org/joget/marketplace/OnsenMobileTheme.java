ca89812d1d599c4e78ae2d50ab3775cb687a9c27
miaoxng <miaoxuan.ng@joget.com>
2025-04-08 15:36:53 +0800

    Fixed: width of buttons and page exceed boundaries when screen size is big

--- src/main/java/org/joget/marketplace/OnsenMobileTheme.java
+++ src/main/java/org/joget/marketplace/OnsenMobileTheme.java
@@ -51,7 +51,7 @@ public class OnsenMobileTheme extends AjaxUniversalTheme{
 
     @Override
     public String getVersion() {
-        return "8.0.4";
+        return "8.0.5";
     } 
 
     @Override
